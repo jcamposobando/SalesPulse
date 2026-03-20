26f0ac562abf63992fb5db6860d2de6f4e9a7037
Juan Campos <juanignacioco@gmail.com>
2026-03-20 12:43:17 -0600

    Add sideback scaffolding with shadcn/ui

---
+++ src/components/layouts/AppLayout.tsx
@@ -0,0 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <SidebarProvider>
+          <AppSidebar></AppSidebar>
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
